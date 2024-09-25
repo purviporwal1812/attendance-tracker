4de60bf4409594caef6265ae90afb35cb5367e2b
Purvi Porwal <purviporwal1812@gmail.com>
2024-09-25 18:05:00 +0530

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:5000", // Proxy requests to the backend
-    },
+  build: {
+    outDir: 'dist',
   },
 });
